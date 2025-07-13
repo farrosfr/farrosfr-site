d11b712d93d9dfd7657f9090fcb4b8a5b6ec4ac3
farrosfr <farros.alfatih@gmail.com>
2025-07-13 11:08:55 +0700

    configure for Netlify deployment

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import vercel from '@astrojs/vercel'
+//import vercel from '@astrojs/vercel'
 import AstroPureIntegration from 'astro-pure'
 import { defineConfig } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
+import netlify from '@astrojs/netlify';
 
 // Others
 // import { visualizer } from 'rollup-plugin-visualizer'
@@ -32,7 +33,7 @@ export default defineConfig({
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: vercel(),
+  adapter: netlify(),
   output: 'server',
   // 2. Vercel (static)
   // adapter: vercelStatic(),
