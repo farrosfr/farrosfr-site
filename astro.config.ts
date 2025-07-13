f8c1920c7bc57b9480ad6c984680f8c6d72e4463
farrosfr <farros.alfatih@gmail.com>
2025-07-13 12:12:23 +0700

    update

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,8 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  //site: 'https://astro-pure.js.org',
+  site: 'https://farrosfr.com',
   // base: '/docs',
   trailingSlash: 'never',
 
