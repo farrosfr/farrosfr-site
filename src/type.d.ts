38cf4cc75fa1def68183f6f22a1e8282e9a56cf5
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-08-18 05:42:21 +0700

    upd gtags 2

--- src/type.d.ts
+++ src/type.d.ts
@@ -2,3 +2,11 @@ declare module 'virtual:config' {
   const Config: import('astro-pure/types').ConfigOutput
   export default Config
 }
+
+// Pastikan blok di bawah ini ada
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
