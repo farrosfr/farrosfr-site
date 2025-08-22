8feaf2c861c495d6505a10b31e89823fa8cd4d94
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-08-22 07:21:33 +0700

    update astro

--- astro.config.ts
+++ astro.config.ts
@@ -35,8 +35,9 @@ export default defineConfig({
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: netlify(),
-  output: 'server',
+  // adapter: netlify(),
+  output: 'static',
+
   // 2. Vercel (static)
   // adapter: vercelStatic(),
   // 3. Local (standalone)
